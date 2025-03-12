84adb4080c64e59ec03517745b843137b29a2501
Jose Daza <jose.daza@outlook.com>
2025-03-12 19:51:56 -0400

    Initial commit

---
+++ backend/models/pedido.js
@@ -0,0 +1,12 @@
+module.exports = (sequelize, DataTypes) => {
+  const Pedido = sequelize.define("Pedido", {
+    fecha: DataTypes.DATE,
+    total: DataTypes.FLOAT
+  });
+
+  Pedido.associate = (models) => {
+    Pedido.belongsTo(models.Cliente, { foreignKey: "clienteId" });
+  };
+
+  return Pedido;
+};
